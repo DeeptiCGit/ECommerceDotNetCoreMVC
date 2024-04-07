74516585b587cdc92b3a8fcc13c738b7d9ff62d5
dchakraw <dchakraw@DCHAKRAW-GO-STB>
2024-04-07 21:16:11 +0530

    Add project files.

---
+++ ECommerceDotNetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECommerceDotNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
